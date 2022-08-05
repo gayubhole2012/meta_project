0d9a44f8725ddf46a16cda93bef60a4a8b98aa15
Gayatri <gayubhole2012@gmail.com>
2022-08-05 10:38:31 -0700

    practice conditional st

--- javascript/variable.js
+++ javascript/variable.js
@@ -132,3 +132,30 @@ switch(place) {
         console.log("The value of the age variable is not numerical");
     }  //output: Each month you get a salary.
     
+    var day = 'hghg';
+    switch(day) {
+        case 'Monday':
+        console.log('Do Something');
+        break;
+        case 'Tuesday':
+        console.log('Do Something');
+        break;
+        case 'Wednesday':
+        console.log('Do Something');
+        break;
+        case 'Thursday':
+        console.log('Do Something');
+        break;
+        case 'Friday':
+        console.log('Do Something');
+        break;
+        case 'Saturday':
+        console.log('Do Something');
+        break;
+        case 'Sunday':
+        console.log('Do Something');
+        break;
+        default:
+        console.log('There is no such day');
+    
+    } // output:There is no such day
