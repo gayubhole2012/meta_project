a0a4e9d6c2739a77ae954426965a75c3c81abf2a
Gayatri <gayubhole2012@gmail.com>
2022-08-07 13:34:39 -0700

    practice function using for and if loop

--- javascript/function.js
+++ javascript/function.js
@@ -39,5 +39,6 @@ function listArrayItems(arr) {
   }
 }
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
-listArrayItems(colors);
+listArrayItems(colors); //output: 0 tomato! 100 orange 200 yellow 300 green 400 blue 500 purple 600 pink
+
 
