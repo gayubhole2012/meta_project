af67f031d6a86b708d8f83bb7a2e58b763a9871f
Gayatri <gayubhole2012@gmail.com>
2022-08-08 16:25:12 -0700

    array are object

--- javascript/object.js
+++ javascript/object.js
@@ -6,7 +6,7 @@
 }         // created an object and assign some value
 
 
-assistantManager.nextachivement = "get promoted";  // you can add using . notaion */
+assistantManager.nextachivement = "get promoted";  // you can add using . notaion 
 
 var assistantManager = {
   rangeTilesPerTurn: 3,
@@ -32,3 +32,63 @@ console.log(house);        //output: { rooms: 3, color: 'brown', priceUSD: 10000
 house.window =10;
 console.log(house);         // output: { rooms: 3, color: 'brown', priceUSD: 10000, window: 10 }
 
+//build objects using the brackets notation
+var house3 = {}
+house3["rooms"] = 4;
+house3["colors"]= "pink";
+house3["priceUSD"]= 234;
+console.log(house3);  // output:{ rooms: 4, colors: 'pink', priceUSD: 234 }
+
+
+var car = {};
+car.color = "red";
+car["color"] = "green";
+car["speed"] = 200;
+car.speed = 100;
+console.log(car); // output:{ color: 'green', speed: 100 }
+
+
+
+var arrOfKeys = ['speed', 'altitude', 'color'];
+var drone = {
+    speed: 100,
+    altitude: 200,
+    color: "red"
+}
+for (var i = 0; i < arrOfKeys.length; i++) {
+    console.log(drone[arrOfKeys[i]]);   // output: 100  200  red
+    
+}
+
+
+//array are object
+var fruit = [];
+fruit.push('apple');
+fruit.push('pear');
+fruit.push('grapes');
+console.log(fruit);     //output: [ 'apple', 'pear', 'grapes' ]
+fruit.pop();
+console.log(fruit);   //output: [ 'apple', 'pear' ]
+
+
+
+/*function arrayBuilder(one,two,three){
+  var arr = [];
+  arr.push('one');
+  arr.push('two');
+  arr.push('three');
+  //console.log(arr); // output:[ 'one', 'two', 'three' ]
+ 
+}*/
+
+function arrayBuilder(one,two,three) {
+  var arr = [];
+  arr.push(one);
+  arr.push(two);
+  arr.push(three);
+  return arr;
+}
+var simpleArr =arrayBuilder('apple', 'pear', 'plum');
+var simpleArr2 = arrayBuilder('wedd','asee','seded');
+console.log(simpleArr); 
+console.log(simpleArr2);    // output: [ 'apple', 'pear', 'plum']    [ 'wedd', 'asee', 'seded' ]
