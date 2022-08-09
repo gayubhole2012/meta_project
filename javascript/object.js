8aa2d8d83eaee5a3661e262fe5a5d41ab33d2b51
Gayatri <gayubhole2012@gmail.com>
2022-08-09 13:57:21 -0700

    practice

--- javascript/object.js
+++ javascript/object.js
@@ -163,3 +163,34 @@ console.log(car);
 car.turnTheKey();
 car.lightsOn();
 
+// typeOf 
+//var test = typeof('what is this?');
+//var test = typeof(123);
+//var test = typeof(3.34);
+//var test = typeof(true);
+//var test = typeof(false);
+//var test = typeof(1<3);
+//var test = typeof([1,2,3]);  // output: object ........arrays in javascript is are object
+var test = typeof({ firstproperty: 1 });
+//var test = typeof(function abc(){console.log('abc')});
+console.log(test);
+
+
+var clothes = [];
+clothes.push('gray t-shirt');
+clothes.push('green scarf');
+clothes.pop();
+clothes.push('slippers');
+clothes.pop();
+clothes.push('boots');
+clothes.push('old jeans');
+console.log(clothes.length);
+
+
+var dog = {
+  color: "brown",
+  height: 30,
+  length: 60
+};
+dog["type"] = "corgi";
+console.log(dog);
