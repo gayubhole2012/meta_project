c365c4d63b70741beacbb91c88d5a23edab97b95
Gayatri <gayubhole2012@gmail.com>
2022-08-10 11:02:06 -0700

    functional programing

---
+++ javascript/functionalprogramming.js
@@ -0,0 +1,27 @@
+/*var currencyOne = 100;
+var currencyTwo = 0;
+exchageRate = 1.2;
+function covertCuerrency(amount,rate){
+  return amount*rate;
+}
+currencyTwo = covertCuerrency(currencyOne,exchageRate);
+console.log(currencyTwo);
+
+console.log('Hello');
+
+//recursion function
+function example(){
+  console.log('line one');
+  console.log('line two');
+  console.log('line three');
+}
+example();
+
+var counter = 3;     //A function that calls itself is called a recursive function.
+function example(){
+  console.log(counter);
+   counter = counter - 1;
+  if(counter === 0)return; 
+  example();
+ }
+  example();*/
