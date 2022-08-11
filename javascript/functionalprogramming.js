ad20b022c47a6027e9ab9940a608b28055f30c48
Gayatri <gayubhole2012@gmail.com>
2022-08-11 09:25:04 -0700

    practice

--- javascript/functionalprogramming.js
+++ javascript/functionalprogramming.js
@@ -26,42 +26,10 @@ function example(){
  }
   example();*/
 
-  // Task 1: Build a function-based console log message generator
-/*function consoleStyler(color,background,fontSize,txt) {
-let message = "%c" + txt;
-let style = `color: ${color};`
-  style += `background: ${background};`
-  style += `font-size: ${fontSize};`
-  console.log(message,style);
-  }
 
-// Task 2: Build another console log message generator
-function celebrateStyler(reason) {
-  //if (typeof reason === "string") {
-    var fontStyle = "color: tomato; font-size: 50px";
-  //}
-  /* if(reason == "birthday") {
-       console.log(`%cHappy birthday`, fontStyle);
-  }
-  else if (reason == "champions") {
-      console.log(`%cCongrats on the title!`, fontStyle);
-  } else {
-      console.log(message, style); 
-  }
   
-}
-consoleStyler(`#1d5c63`, `#ede6db`, `40px`, `Congrats!`);
-celebrateStyler(`birthday`);
-// Task 3: Run both the consoleStyler and the celebrateStyler functions
-// Task 4: Insert a congratulatory and custom message
 
-function styleAndCelebrate(color, background, fontSize, txt,reason) {
-  //if(typeof reason ==="string")
-  consoleStyler(color, background, fontSize, txt); 
-  celebrateStyler(reason);   
-}
-styleAndCelebrate( 'ef7c8e','fae8e0', '30px', 'You made it!', 'champions');
-// Call styleAndCelebrate*/
+
 
 
 // Task 1: Build a function-based console log message generator
@@ -96,7 +64,7 @@ function styleAndCelebrate(color, background, fontSize, txt, reason) {
   celebrateStyler(reason);
 }
 // Call styleAndCelebrate
-styleAndCelebrate('ef7c8e', 'fae8e0', '30px', 'You made it', 'champions');*/
+styleAndCelebrate('ef7c8e', 'fae8e0', '30px', 'You made it', 'champions');
 
 
 
@@ -109,17 +77,19 @@ function scopeTest() {
 console.log(localVar);*/ // undefined
 
 
-function meal(animal) {
+/*function meal(animal) {
   animal.food = animal.food + 10;
 }
-/*
+
 var dog = {
   food: 10
 };
 meal(dog);
 meal(dog);
 
-console.log(dog.food);*/
+console.log(dog.food);
+
+
 
 function two() {
   return 2;
@@ -131,6 +101,16 @@ function one() {
 
 function calculate(initialValue, incrementValue) {
   return initialValue() + incrementValue() + incrementValue();
-}
+}*/
 
-console.log(calculate(two, one)); // 4
+//recursion
+/*console.log(calculate(two, one)); // 4 */
+
+function num(b){
+ console.log(b);
+ let c= b-1;
+ if(c > 0){
+  num(c);
+ }
+}
+num(7); 
